111e7a4c75426e21a8dc5fd148a095fb1336c4d6
mowebiaz <mowebiaz@gmail.com>
2026-01-27 10:31:56 +0100

    chore: add missing file
    for global-not-found page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 import { withPayload } from '@payloadcms/next/withPayload'
 
 const nextConfig: NextConfig = {
+  experimental: {
+    globalNotFound: true,
+  },
   images: {
     remotePatterns: [
       {
