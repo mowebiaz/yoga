834f3af69747794f956c72e8dd0b92a0056bc21a
mowebiaz <mowebiaz@gmail.com>
2026-01-28 09:33:37 +0100

    Addi custom colors to the RichText component

--- src/components/RichText/converters/textConverter.tsx
+++ src/components/RichText/converters/textConverter.tsx
@@ -9,37 +9,53 @@ import { CSSProperties } from 'react'
 
 export const colorState: TextStateFeatureProps['state'] = {
   color: {
-    ...defaultColors.text,
-    black: {
-      label: 'Black',
+    //...defaultColors.text,
+    text: {
+      label: 'Violet',
       css: {
-        color: '#000000',
+        color: '#3e1d40',
       },
     },
+    secondary: {
+      label: 'Saumon',
+      css: {
+        color: '#faa98c'
+      }
+    },
+    accent: {
+      label: 'Bleu',
+      css: {
+        color: '#8ea6d7'
+      }
+    }
+
   },
   background: {
-    ...defaultColors.background,
-    greenmw: {
-      label: 'Green - MW',
+    //...defaultColors.background,
+    primarybkg: {
+      label: 'Violet',
       css: {
-        'background-color': '#00ECAE',
+        'background-color': '#532755',
       },
     },
+    secondarybkg: {
+      label: 'Saumon',
+      css: {
+        'background-color': '#faa98c'
+      }
+    },
+    accentbkg: {
+      label: 'Bleu',
+      css: {
+        'background-color': '#8ea6d7'
+      }
+    }
   },
   size: {
     large: {
       label: 'Large text',
       css: {
         'font-size': 'large',
-      },
-    },
-  },
-  underline: {
-    dashed: {
-      label: 'Dashed',
-      css: {
-        'text-decoration': 'underline dashed',
-        'text-underline-offset': '4px',
       },
     },
   },
