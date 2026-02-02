0d7363a8d814a8803c6cc7178a3d807b855b6dee
mowebiaz <mowebiaz@gmail.com>
2026-02-02 14:37:05 +0100

    migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260130_090151 from './20260130_090151';
 import * as migration_20260131_151321 from './20260131_151321';
+import * as migration_20260202_133558 from './20260202_133558';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260131_151321.up,
     down: migration_20260131_151321.down,
-    name: '20260131_151321'
+    name: '20260131_151321',
+  },
+  {
+    up: migration_20260202_133558.up,
+    down: migration_20260202_133558.down,
+    name: '20260202_133558'
   },
 ];
