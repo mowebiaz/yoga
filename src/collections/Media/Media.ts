37d62ef624f1fc6ad51eead487a2225181a984e9
mowebiaz <mowebiaz@gmail.com>
2026-04-04 18:50:02 +0200

    fix: add adminThumbnail function to generate thumbnail URL from media data

--- src/collections/Media/Media.ts
+++ src/collections/Media/Media.ts
@@ -3,6 +3,15 @@ import { anyone } from '../Users/access/anyone'
 import editor from '../Users/access/editor'
 import user from '../Users/access/user'
 
+type MediaForThumb = {
+  url?: string | null
+  sizes?: {
+    thumbnail?: {
+      filename?: string | null
+    }
+  }
+}
+
 export const Media: CollectionConfig = {
   slug: 'media',
   access: {
@@ -60,7 +69,15 @@ export const Media: CollectionConfig = {
     },
     mimeTypes: ['image/*'],
 
-    adminThumbnail: 'thumbnail',
+        adminThumbnail: ({ doc }) => {
+      const media = doc as MediaForThumb
+      const thumbFilename = media?.sizes?.thumbnail?.filename
+      if (thumbFilename) {
+        return `${process.env.BLOB_PUBLIC_URL}/${thumbFilename}`
+      }
+
+      return media?.url ?? null
+    },
     imageSizes: [
       {
         name: 'thumbnail',
